21596cc9eee03e7d4fa1176c1a802e63c4c5be56
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-10-21 23:43:39 +0700

    #49 custom react slider carousel

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -8,32 +8,58 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import specialtyImg from "../../../assets/specialtyimage/co-xuong-khop.jpg"
+
 class Specialty extends Component {
     render() {
         let setting = {
             dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 4,
+            slidesToScroll: 1,
         };
         return (
             <div className='section-specialty'>
-                <div className='specialty-content'>
-                    <Slider {...setting}>
-                        <div className='img-customize'>
-                            <img src="https://static.wixstatic.com/media/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png/v1/fill/w_256,h_256,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png" />
-                        </div>
-                        <div className='img-customize'>
-                            <img src="https://static.wixstatic.com/media/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png/v1/fill/w_256,h_256,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png" />
-                        </div>
-                        <div className='img-customize'>
-                            <img src="https://static.wixstatic.com/media/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png/v1/fill/w_256,h_256,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png" />
-                        </div >
-                        <div className='img-customize'>
-                            <img src="https://static.wixstatic.com/media/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png/v1/fill/w_256,h_256,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/d7b33b_5e70ab116039449eacf86128ea83e742~mv2.png" />
-                        </div>
-                    </Slider>
+                <div className='specialty-container'>
+                    <div className='specialty-header'>
+                        <span className='title-section'>Chuyên Khoa Phổ Biến</span>
+                        <button className='btn-section'>Xem thêm...</button>
+                    </div>
+                    <div className='specialty-body'>
+                        <Slider {...setting}>
+                            <div className='specialty-customize'>
+                                <div className='bg-img'> </div>
+                                <div>Cơ Xương Khớp </div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-img--viemgan'> </div>
+                                <div>Bệnh Viêm Gan</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-img--thankinh'> </div>
+                                <div>Thần Kinh</div>
+                            </div >
+                            <div className='specialty-customize'>
+                                <div className='bg-img--timmach'> </div>
+                                <div>Tim Mạch</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-img--tieuhoa'> </div>
+                                <div>Tiêu Hóa</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-img--viemphoi'> </div>
+                                <div>Hô Hấp - Phổi</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-img--tamthan'> </div>
+                                <div>Sức Khỏe Tâm Thần</div>
+                            </div>
+
+                        </Slider>
+                    </div>
+
                 </div>
             </div >
         );
