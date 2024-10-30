9ed98fe6dd3410e42fc25fd9c41fd4d2066f919b
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-10-30 23:13:52 +0700

    #55 Hoan Thien Menu Phan quyen phia Admin

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -7,7 +7,7 @@ import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils/constant';
 import { lang } from 'moment';
-
+import { FormattedMessage } from 'react-intl';
 class Header extends Component {
 
     handleChangeLanguage = (language) => {
@@ -16,7 +16,8 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
+        console.log('check userInfo', userInfo)
 
         return (
             <div className="header-container">
@@ -26,6 +27,8 @@ class Header extends Component {
                 </div>
 
                 <div className='languages'>
+                    <span className='welcome'><FormattedMessage id='home-header.welcome' />
+                        {userInfo && userInfo.firstName ? userInfo.firstName : ''} !</span>
                     <span className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}>
                         VN</span>
@@ -49,6 +52,7 @@ const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
         language: state.app.language,
+        userInfo: state.user.userInfo
 
     };
 };
